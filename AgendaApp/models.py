ce1063694e6d6b0741df6912b7e8d2ad6bbfab12
Fernanda Nunes <nunesfnds546@gmail.com>
2023-10-30 21:53:35 -0300

    Inclusão do campo Estado Civil

--- AgendaApp/models.py
+++ AgendaApp/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Agenda(models.Model):
+
+    ESTADO_CIVIS = [
+        ('S', 'Solteiro'), 
+        ('C', 'Casado'), 
+        ('D', 'Divorciado'), 
+        ('V', 'Viúvo')
+        ]
     nome = models.CharField(max_length=100)
     apelido = models.CharField(max_length=30)
     email = models.EmailField(max_length=100)
@@ -13,6 +20,7 @@ class Agenda(models.Model):
     bairro = models.CharField(max_length=100)
     cidade = models.CharField(max_length=100)
     estado = models.CharField(max_length=50)
+    estado_civil = models.CharField(max_length=1, choices=ESTADO_CIVIS, null=True)
 
     def __str__(self):
         return self.nome
